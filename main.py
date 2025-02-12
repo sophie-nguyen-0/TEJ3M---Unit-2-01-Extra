232aedf63efa78ff3da16facb5f5b23daae5491e
Sophie Nguyen <113055711+sophie-nguyen-0@users.noreply.github.com>
2025-02-12 09:59:19 -0500

    comments

--- main.py
+++ main.py
@@ -3,11 +3,16 @@ import time
 import board
 import digitalio
 
+#variables
 led = digitalio.DigitalInOut(board.LED)
 led.direction = digitalio.Direction.OUTPUT
 
+# loop
 while True:
+    #turns on LED
     led.value = True
+    #wait 1 second
     time.sleep(1)
+    #turns LED off
     led.value = False
     time.sleep(1)
